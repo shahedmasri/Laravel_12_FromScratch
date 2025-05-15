09d8651c7a5153b9bb0beaf878ec087e994b6923
shahed <shahed@example.com>
2025-05-15 23:08:59 +0300

    Push lesson 1 & 2

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/second', function () {
+    return view('second');
+});
+
+//Route::view('/second', 'second');
+Route::view('/', 'home');
