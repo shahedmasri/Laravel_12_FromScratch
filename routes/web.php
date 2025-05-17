6e53366db02ff2478f4b75251cc82d743f387788
shahed <shahed@example.com>
2025-05-18 01:55:07 +0300

    MVC, DB Queries, and Eloquent Models

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,6 +13,7 @@ Route::get('/second', function () {
 
 //Route::view('/second', 'second');
 Route::view('/', 'home')->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
 Route::view('article', 'article')->name('article');
