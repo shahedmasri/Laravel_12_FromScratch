7391ac16553f4227925e4cc63f113d0e5d409bf6
shahed <shahed@example.com>
2025-05-18 02:29:30 +0300

    Eloquent Relations and GET Parameters

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use App\Models\Category;
+use App\Models\Post;
 
 use Illuminate\Http\Request;
 
@@ -11,6 +12,11 @@ class HomeController extends Controller
     public function index()
   {
       $categories = Category::all();
-      return view('home', ['categories' => $categories]);
+      $posts = Post::when(request('category_id'), function ($query) {
+          $query->where('category_id', request('category_id'));
+      })->latest()->get();
+      // return view('home', ['categories' => $categories, 'posts' => $posts]);
+      //This function can be used if the names of the variables are the same as array keys.
+      return view('home', compact('categories', 'posts'));
   }
 }
